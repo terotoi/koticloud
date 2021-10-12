a8960617baf7a7154399d8aa09ae5d44e29fd415
Tero Oinas <tero@dalen.co>
2021-10-12 19:20:34 +0300

    Allow admins access to access all nodes.

--- server/fs/access.go
+++ server/fs/access.go
@@ -4,5 +4,5 @@ import "github.com/terotoi/koticloud/server/models"
 
 // Checks if the given user has access to the given node.
 func AccessAllowed(user *models.User, node *models.Node, write bool) bool {
-	return node.OwnerID.Valid && node.OwnerID.Int == user.ID
+	return user.Admin || (node.OwnerID.Valid && node.OwnerID.Int == user.ID)
 }
